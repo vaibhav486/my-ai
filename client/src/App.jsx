39732ade18c46aa48d3a9d0bc2c9428cf7c906fe
vaibhav <vaibh499@gmail.com>
2025-09-30 17:12:47 +0530

    link repair

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import WriteArticle from './pages/WriteArticle'
 import BlogTitles from './pages/BlogTitles'
